8ad677136ff7b4d7f85dd5024140c3690d05e0a7
Amit Kumar <amitgodara1008@gmail.com>
2024-06-03 00:40:55 +0530

    add name validation

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -6,7 +6,13 @@ import { log } from "../utils/logger";
 export const createUser = (req: Request, res: Response) =>
   Effect.gen(function* () {
     const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: "Missing required field: name" });
+    }
+
     const repo = yield* UserRepository;
+
     const user = yield* repo.createUser(name);
     log(`User created: ${user.id}`);
     return res.json(user);
